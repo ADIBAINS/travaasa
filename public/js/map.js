2bbab1a727103de724bbd2ae6bf806e55b14ddbe
ADIL BAINS <adibains06@gmail.com>
2025-08-16 21:05:08 +0530

    Multer version change

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,4 @@
 mapboxgl.accessToken = MAP_TOKEN;
-console.log(MAP_TOKEN);
-
-
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
